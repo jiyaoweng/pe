f48c66b477d497bbe61e3ded5028eab01ebce8cb
jiyao weng <jiyaoweng@jiyaos-macbook-pro-2.local>
2016-01-20 21:24:11 -0500

    Initial Commit

---
+++ photoeffects/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  photoeffects
+//
+//  Created by jiyao weng on 2016-01-20.
+//  Copyright © 2016 jiyao weng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
